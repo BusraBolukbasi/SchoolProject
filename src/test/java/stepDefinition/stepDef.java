ca18076b43ca8cb15bda0978bbf5af2645e533b5
BusraBolukbasi <brblk.58@gmail.com>
2023-08-31 20:47:09 +0300

    30/08/23

---
+++ src/test/java/stepDefinition/stepDef.java
@@ -0,0 +1,7 @@
+package stepDefinition;
+
+public class stepDef {
+    public static void main(String[] args) {
+        System.out.println("Simdilik calismalar baslamadigi icin bu sekilde gonderim olacak :D");
+    }
+}
