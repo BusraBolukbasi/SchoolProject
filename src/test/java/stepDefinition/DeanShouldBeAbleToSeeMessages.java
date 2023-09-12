0f335c6e1b29c577f58f6f54464e1279dab58463
Busra Bolukbasi <brblk.58@gmail.com>
2023-09-12 15:08:46 +0300

    12/09/23

--- src/test/java/stepDefinition/DeanShouldBeAbleToSeeMessages.java
+++ src/test/java/stepDefinition/DeanShouldBeAbleToSeeMessages.java
@@ -13,13 +13,36 @@ public class DeanShouldBeAbleToSeeMessages {
     @When("Giris yaptiktan sonra sag ustte bulunan menu butonuna tiklar ve contact get all butonunu secer")
     public void giris_yaptiktan_sonra_sag_ustte_bulunan_menu_butonuna_tiklar_ve_contact_get_all_butonunu_secer() {
         deanManagmentPage.menuVeContactGetAllButonu();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
 
     }
     @Then("Mesajlarin gorunur oldugunu dogrula")
     public void mesajlarin_gorunur_oldugunu_dogrula() {
         softAssert.assertTrue(deanManagmentPage.messages.isEmpty());
-
-
     }
 
 }
